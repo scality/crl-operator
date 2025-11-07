195d371221b202c139d80872c984d0c40ac8bb65
Teddy Andrieux <teddy.andrieux@scality.com>
2025-11-07 10:54:56 +0000

    chore: Add logic and test for validation webhook
    Signed-off-by: Teddy Andrieux <teddy.andrieux@scality.com>

--- internal/webhook/v1alpha1/managedcrl_webhook.go
+++ internal/webhook/v1alpha1/managedcrl_webhook.go
@@ -20,12 +20,15 @@ import (
 	"context"
 	"fmt"
 
+	cmv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
+	"github.com/go-logr/logr"
 	crloperatorv1alpha1 "github.com/scality/crl-operator/api/v1alpha1"
 )
 
@@ -36,7 +39,9 @@ var managedcrllog = logf.Log.WithName("managedcrl-resource")
 // SetupManagedCRLWebhookWithManager registers the webhook for ManagedCRL in the manager.
 func SetupManagedCRLWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).For(&crloperatorv1alpha1.ManagedCRL{}).
-		WithValidator(&ManagedCRLCustomValidator{}).
+		WithValidator(&ManagedCRLCustomValidator{
+			client: mgr.GetClient(),
+		}).
 		Complete()
 }
 
@@ -53,35 +58,34 @@ func SetupManagedCRLWebhookWithManager(mgr ctrl.Manager) error {
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as this struct is used only for temporary operations and does not need to be deeply copied.
 type ManagedCRLCustomValidator struct {
-	// TODO(user): Add more fields as needed for validation
+	client client.Client
 }
 
 var _ webhook.CustomValidator = &ManagedCRLCustomValidator{}
 
 // ValidateCreate implements webhook.CustomValidator so a webhook will be registered for the type ManagedCRL.
-func (v *ManagedCRLCustomValidator) ValidateCreate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
+func (v *ManagedCRLCustomValidator) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
 	managedcrl, ok := obj.(*crloperatorv1alpha1.ManagedCRL)
 	if !ok {
 		return nil, fmt.Errorf("expected a ManagedCRL object but got %T", obj)
 	}
-	managedcrllog.Info("Validation for ManagedCRL upon creation", "name", managedcrl.GetName())
+	logger := managedcrllog.WithValues("name", managedcrl.GetName()).WithValues("namespace", managedcrl.GetNamespace())
 
-	// TODO(user): fill in your validation logic upon object creation.
+	logger.Info("Validation for ManagedCRL upon creation")
 
-	return nil, nil
+	return nil, validationManagedCRL(logger, ctx, v.client, managedcrl)
 }
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type ManagedCRL.
-func (v *ManagedCRLCustomValidator) ValidateUpdate(_ context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
+func (v *ManagedCRLCustomValidator) ValidateUpdate(ctx context.Context, oldObj, newObj runtime.Object) (admission.Warnings, error) {
 	managedcrl, ok := newObj.(*crloperatorv1alpha1.ManagedCRL)
 	if !ok {
 		return nil, fmt.Errorf("expected a ManagedCRL object for the newObj but got %T", newObj)
 	}
-	managedcrllog.Info("Validation for ManagedCRL upon update", "name", managedcrl.GetName())
+	logger := managedcrllog.WithValues("name", managedcrl.GetName()).WithValues("namespace", managedcrl.GetNamespace())
+	logger.Info("Validation for ManagedCRL upon update")
 
-	// TODO(user): fill in your validation logic upon object update.
-
-	return nil, nil
+	return nil, validationManagedCRL(logger, ctx, v.client, managedcrl)
 }
 
 // ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type ManagedCRL.
@@ -95,4 +99,50 @@ func (v *ManagedCRLCustomValidator) ValidateDelete(ctx context.Context, obj runt
 	// TODO(user): fill in your validation logic upon object deletion.
 
 	return nil, nil
+}
+
+// validationManagedCRL validates the ManagedCRL fields.
+func validationManagedCRL(logger logr.Logger, ctx context.Context, c client.Client, managedcrl *crloperatorv1alpha1.ManagedCRL) error {
+	managedcrl.WithDefaults()
+	if err := managedcrl.Validate(); err != nil {
+		logger.Error(err, "Validation failed")
+		return err
+	}
+
+	// Ensure the specified Issuer or ClusterIssuer exists
+	issuerLogger := logger.WithValues("issuer_name", managedcrl.Spec.IssuerRef.Name, "issuer_kind", managedcrl.Spec.IssuerRef.Kind)
+	issuerRef := managedcrl.Spec.IssuerRef
+	switch issuerRef.Kind {
+	case "Issuer":
+		var issuer cmv1.Issuer
+		err := c.Get(ctx, client.ObjectKey{Namespace: managedcrl.Namespace, Name: issuerRef.Name}, &issuer)
+		if err != nil {
+			issuerLogger.Error(err, "Issuer not found")
+			return fmt.Errorf("issuer %s not found in namespace %s", issuerRef.Name, managedcrl.Namespace)
+		}
+		if issuer.Spec.CA == nil || issuer.Spec.CA.SecretName == "" {
+			err := fmt.Errorf("issuer %s in namespace %s is not a CA issuer", issuerRef.Name, managedcrl.Namespace)
+			issuerLogger.Error(err, "Issuer is not a CA issuer")
+			return err
+		}
+	case "ClusterIssuer":
+		var issuer cmv1.ClusterIssuer
+		err := c.Get(ctx, client.ObjectKey{Name: issuerRef.Name}, &issuer)
+		if err != nil {
+			issuerLogger.Error(err, "ClusterIssuer not found")
+			return fmt.Errorf("clusterissuer %s not found", issuerRef.Name)
+		}
+		if issuer.Spec.CA == nil || issuer.Spec.CA.SecretName == "" {
+			err := fmt.Errorf("clusterissuer %s is not a CA issuer", issuerRef.Name)
+			issuerLogger.Error(err, "Issuer is not a CA issuer")
+			return err
+		}
+	default:
+		err := fmt.Errorf("invalid IssuerRef kind: %s", issuerRef.Kind)
+		issuerLogger.Error(err, "IssuerRef kind must be either 'Issuer' or 'ClusterIssuer'")
+		return err
+	}
+
+	logger.Info("Validation successful")
+	return nil
 }
